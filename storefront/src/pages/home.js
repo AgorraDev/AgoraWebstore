603b203c4fd85da26ce95e3b4b2b8b159b429a27
agorra <agorradev@gmail.com>
2024-02-17 17:56:57 +0000

    .Fixed <Footer /> positioning     -Now sits at page bottom         - footer.css=d-block/position-absolute         - (previous)container=position-relative

--- storefront/src/pages/home.js
+++ storefront/src/pages/home.js
@@ -13,7 +13,7 @@ function Home() {
            {/*Bootstrap link*/}
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"crossorigin="anonymous"/>
            {/*Links End*/}
-        <Navigation></Navigation>
+        <Navigation />
         <Container fluid className="hero-bg-img position-relative" load="lazy">
 
                 <Container fluid>
@@ -33,7 +33,7 @@ function Home() {
                 </div>
                 </Container>
         </Container>
-        <Container fluid className='hero2-bg' loading="lazy">
+        <Container fluid className='hero2-bg position-relative' loading="lazy">
           <div className="container col-xxl-12 px-3">
             <div className="row g-3">
               <div className="col-6 hero2-content-img">
@@ -51,7 +51,7 @@ function Home() {
 
           </div>
         </Container>
-        <Footer></Footer>
+        <Footer />
         </>
     );
 }
